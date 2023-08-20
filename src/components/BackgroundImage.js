26b0726c30bbb1ee63452b5e6ea9596520d52b63
Spacedaway <cancokeforreal@yahoo.com>
2023-08-20 22:41:17 +0100

    Fixed Background Image

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -1,6 +1,5 @@
 import { Image, View, StyleSheet } from 'react-native';
 import React from 'react';
-import Option from './Option';
 
 const BackgroundImage = ({ size, src }) => {
 	return (
@@ -20,9 +19,7 @@ export default BackgroundImage;
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
 		justifyContent: 'center',
-		alignItems: 'center',
-		marginTop: -250,
+		marginTop: 125,
 	},
 });
