4e87bd62e3c8bf264fcaecbb4dd36bec4e0c1c4d
Spacedaway <cancokeforreal@yahoo.com>
2023-08-21 17:37:27 +0100

    Minor Fixes

--- src/screens/RockPaperScissors.js
+++ src/screens/RockPaperScissors.js
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import React, { useState } from 'react';
 import ScoreBoard from '../components/ScoreBoard';
