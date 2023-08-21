a41541f5815ca3fc9ffde24712ae814b2a1b1c71
Spacedaway <cancokeforreal@yahoo.com>
2023-08-21 01:13:38 +0100

    Added ScoreBoard to GamePlay

--- src/screens/GamePlay.js
+++ src/screens/GamePlay.js
@@ -1,14 +1,36 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+import { useRoute } from '@react-navigation/native';
+import ScoreBoard from '../components/ScoreBoard';
+import useScreenWidth from '../hooks/useScreenWidth';
 
 const GamePlay = () => {
+	const [screenWidth] = useScreenWidth();
+	const route = useRoute();
+	const { option, routeName } = route.params;
+	console.log(routeName);
+
+	if (routeName === 'Advance') {
+		source = require('../../assets/images/logo-bonus.png');
+	} else {
+		source = require('../../assets/images/logo.png');
+	}
+
 	return (
-		<View>
-			<Text>GamePlay</Text>
-		</View>
+		<SafeAreaView style={styles.container}>
+			<ScoreBoard src={source} size={screenWidth * 0.2} />
+		</SafeAreaView>
 	);
 };
 
 export default GamePlay;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: 'hsl(214, 47%, 23%)',
+		alignItems: 'center',
+	},
+});
