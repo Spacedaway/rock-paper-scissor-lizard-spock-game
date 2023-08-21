3ad7328504e9d9e914cec905f7297782a8ad69a1
Spacedaway <cancokeforreal@yahoo.com>
2023-08-21 18:40:56 +0100

    Updated Scores Functions and Payload

--- src/context/ScoresContext.js
+++ src/context/ScoresContext.js
@@ -2,21 +2,52 @@ import createDataContext from './createDataContext';
 
 const scoresReducer = (state, action) => {
 	switch (action.type) {
-		case value:
-			break;
+		case 'win':
+			return {
+				...state,
+				[action.payload]: {
+					...state[action.payload],
+					win: state[action.payload].win + 1,
+				},
+			};
+		case 'draw':
+			return state;
+		case 'loss':
+			return {
+				...state,
+				[action.payload]: {
+					...state[action.payload],
+					win: state[action.payload].win - 1,
+				},
+			};
 		default:
 			return state;
 	}
 };
 
 const win = (dispatch) => {
-	return (basicMode) => {
-		basicMode
-			? dispatch({ type: 'basic_win' })
-			: dispatch({ type: 'basic_win' });
+	return (gameMode) => {
+		dispatch({ type: 'win', payload: gameMode });
 	};
 };
 
-export const { Provider, Context } = createDataContext({
+const draw = (dispatch) => {
+	return (gameMode) => {
+		dispatch({ type: 'draw', payload: gameMode });
+	};
+};
+
+const loss = (dispatch) => {
+	return (gameMode) => {
+		dispatch({ type: 'loss', payload: gameMode });
+	};
+};
+
+export const { Provider, Context } = createDataContext(
 	scoresReducer,
-});
+	{ win, loss, draw },
+	{
+		Basic: { win: 0, loss: 0, draw: 0 },
+		Advance: { win: 0, loss: 0, draw: 0 },
+	}
+);
